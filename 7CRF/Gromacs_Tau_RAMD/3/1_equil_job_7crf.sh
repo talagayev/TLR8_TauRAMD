79ed389fc2bc0d4b3d83454eee0b778b71ea6429
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-12-22 23:59:43 +0100

    Add files via upload

---
+++ 7CRF/Gromacs_Tau_RAMD/3/1_equil_job_7crf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+#$ -cwd
+#$ -o gro_gpu.$JOB_ID.out
+#$ -N gro_gpu
+#$ -j y
+#SBATCH --job-name=7crf_equil
+#SBATCH -t 2-00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:2
+#$ -l h_rt=720000
+#$ -pe mp 2
+#$ -l gpus=1
+
+module rm ompi
+module load gromacs/ramd-2.0
+
+gmx grompp -f gromacs.mdp -c 7crf_3.gro -o 7crf0.tpr -p 7crf_3.top -maxwarn  5 -n index.ndx
+gmx mdrun -s 7crf0.tpr -c confout_1st_equil.gro -x 1st_equil_run.xtc -g 1st_equil.log -ntmpi 1 -ntomp 16 -maxh 24
+
