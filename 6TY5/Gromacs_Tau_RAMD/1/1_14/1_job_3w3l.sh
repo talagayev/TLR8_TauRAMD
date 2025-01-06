32d7e3722d5c82a7c857144ae602ab7a35fe2dc3
talagayv95@gmail.com <talagayv95@gmail.com>
2025-01-06 16:15:47 +0100

    added 6TY5

---
+++ 6TY5/Gromacs_Tau_RAMD/1/1_14/1_job_3w3l.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+#$ -cwd
+#$ -o gro_gpu.$JOB_ID.out
+#$ -N gro_gpu
+#$ -j y
+#SBATCH --job-name=6ty5_ramd
+#SBATCH -t 2-00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:2
+#$ -l h_rt=576000
+#$ -pe mp 16
+#$ -l gpus=2
+
+module rm ompi
+module load gromacs/ramd-2.0
+
+gmx grompp -f gromacs_ramd_40kcal.mdp -c confout_1st_equil.gro -o 6ty5_1_Rep14_tauramd_run.tpr  -n index.ndx -p 6ty5_1.top  -maxwarn  2
+gmx mdrun -s 6ty5_1_Rep14_tauramd_run.tpr  -c confout_1_14_Rep_tauramd.gro -x confout_1_14_Rep_tauramd.xtc -g 2nd_tauramd.log -maxh 160 > out
+
---
+++ 6TY5/Gromacs_Tau_RAMD/3/1_equil_job_6ty5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+#$ -cwd
+#$ -o gro_gpu.$JOB_ID.out
+#$ -N gro_gpu
+#$ -j y
+#SBATCH --job-name=6ty5_equil
+#SBATCH -t 2-00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:2
+#$ -l h_rt=720000
+#$ -pe mp 2
+#$ -l gpus=1
+
+module rm ompi
+module load gromacs/ramd-2.0
+
+gmx grompp -f gromacs.mdp -c 6ty5_3.gro -o 6ty50.tpr -p 6ty5_3.top -maxwarn  5 -n index.ndx
+gmx mdrun -s 6ty50.tpr -c confout_1st_equil.gro -x 1st_equil_run.xtc -g 1st_equil.log -ntmpi 1 -ntomp 16 -maxh 24
+
